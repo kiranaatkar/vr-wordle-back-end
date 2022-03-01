751f43f1183122c3d7bd952c2ae98cf7ccb18960
Kiran Aatkar <kiranaatkar@gmail.com>
2022-03-01 14:48:57 +0000

    updated seed data to have correct words and dates

--- server.test.js
+++ server.test.js
@@ -1,22 +1,22 @@
 //import assert methods from deno library
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { test, TestSuite } from "https://deno.land/x/test_suite@0.9.5/mod.ts";
-import { sleep } from "https://deno.land/x/sleep/mod.ts";
+import { assertEquals } from 'https://deno.land/std/testing/asserts.ts';
+import { test, TestSuite } from 'https://deno.land/x/test_suite@0.9.5/mod.ts';
+import { sleep } from 'https://deno.land/x/sleep/mod.ts';
 
-const apiURL = "http://localhost:8080";
-const path = "server.js";
+const apiURL = 'http://localhost:8080';
+const path = 'server.js';
 let server = undefined;
 
 async function setupServer() {
   const server = await Deno.run({
-    cmd: ["deno", "run", "--unstable", "-A", path],
+    cmd: ['deno', 'run', '--unstable', '-A', path],
   });
   await sleep(1.5);
   return server;
 }
 
 const suite = new TestSuite({
-  name: "Test on scores db",
+  name: 'Test on scores db',
   async beforeEach() {
     server = await setupServer();
   },
@@ -28,60 +28,60 @@ const suite = new TestSuite({
   },
 });
 
-test(suite, "/scores returns all scores in db", async () => {
+test(suite, '/scores returns all scores in db', async () => {
   const response = await fetch(`${apiURL}/scores`);
   const json = await response.json();
-  assertEquals(json.scores.length, 9);
+  assertEquals(json.scores.length, 63);
 });
 
 // ---------- QUERY PARAMS ---------- //
 
-test(suite, "word query param works correctly", async () => {
+test(suite, 'word query param works correctly', async () => {
   const response = await fetch(`${apiURL}/scores?word=rupee`);
   const json = await response.json();
   const allScoresContainWord = json.scores.every(
-    (score) => score.word === "rupee"
+    (score) => score.word === 'rupee'
   );
   assertEquals(allScoresContainWord, true);
 });
 
-test(suite, "username query param works correctly", async () => {
+test(suite, 'username query param works correctly', async () => {
   const response = await fetch(`${apiURL}/scores?username=mixup692`);
   const json = await response.json();
   const onlyUserScores = json.scores.every(
-    (score) => score.username === "mixup692"
+    (score) => score.username === 'mixup692'
   );
   assertEquals(onlyUserScores, true);
 });
 
-test(suite, "combination of query params works correctly", async () => {
+test(suite, 'combination of query params works correctly', async () => {
   const response = await fetch(`${apiURL}/scores?username=mixup692&word=rupee`);
   const json = await response.json();
   const conditionsMet = json.scores.every((score) => {
-    return score.username === "mixup692" && score.word === "rupee";
+    return score.username === 'mixup692' && score.word === 'rupee';
   });
   assertEquals(conditionsMet, true);
 });
 
-test(suite, "invalid query params throw error", async () => {
+test(suite, 'invalid query params throw error', async () => {
   const response = await fetch(`${apiURL}/scores?invalid=query`);
   const json = await response.json();
   assertEquals(
     json.response,
-    "Invalid query params. Allowed params are username, word or date"
+    'Invalid query params. Allowed params are username, word or date'
   );
 });
 
 // ---------- ADDING SCORE ---------- //
 
-test(suite, "score adds successfully to db", async () => {
-  const [score, word, username] = [2, "choke", "cranwell276"];
+test(suite, 'score adds successfully to db', async () => {
+  const [score, word, username] = [2, 'choke', 'cranwell276'];
 
   // Add score to db
   const postRequest = await fetch(`${apiURL}/scores`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       score,
@@ -92,7 +92,7 @@ test(suite, "score adds successfully to db", async () => {
 
   // Check if new score in db
   const postRequestJson = await postRequest.json();
-  assertEquals(postRequestJson.response, "score successfully added");
+  assertEquals(postRequestJson.response, 'score successfully added');
 
   // Delete test score from db
   const response = await fetch(`${apiURL}/scores`);
@@ -101,21 +101,21 @@ test(suite, "score adds successfully to db", async () => {
     (entry) => entry.username === username
   ).id;
   const deleteRequest = await fetch(`${apiURL}/scores/${newScoreId}`, {
-    method: "DELETE",
-    credentials: "include",
-    headers: { "Content-Type": "application/json" },
+    method: 'DELETE',
+    credentials: 'include',
+    headers: { 'Content-Type': 'application/json' },
   });
   const deleteRequestJson = await deleteRequest.json();
-  assertEquals(deleteRequestJson.response, "score entry deleted");
+  assertEquals(deleteRequestJson.response, 'score entry deleted');
 });
 
-test(suite, "missing entry items throws error", async () => {
-  const [score, word, username] = ["", "", "cranwell276"];
+test(suite, 'missing entry items throws error', async () => {
+  const [score, word, username] = ['', '', 'cranwell276'];
 
   const postRequest = await fetch(`${apiURL}/scores`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       score,
@@ -127,6 +127,6 @@ test(suite, "missing entry items throws error", async () => {
   const postRequestJson = await postRequest.json();
   assertEquals(
     postRequestJson.response,
-    "Item(s) missing, need username, word and score for a valid request."
+    'Item(s) missing, need username, word and score for a valid request.'
   );
 });
