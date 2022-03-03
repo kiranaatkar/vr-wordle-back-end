d48c82e9486243a2993fecb50b392df3b472eea7
Kiran Aatkar <kiranaatkar@gmail.com>
2022-03-03 16:52:44 +0000

    Added order by score to getdata function

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import { Application } from "https://deno.land/x/abc/mod.ts";
-import { abcCors } from "https://deno.land/x/cors/mod.ts";
-import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { Application } from 'https://deno.land/x/abc/mod.ts';
+import { abcCors } from 'https://deno.land/x/cors/mod.ts';
+import { Client } from 'https://deno.land/x/postgres@v0.11.3/mod.ts';
+import { config } from 'https://deno.land/x/dotenv/mod.ts';
 
 //set up database with environment variables
 
-const DENO_ENV = Deno.env.get("DENO_ENV") ?? "development";
+const DENO_ENV = Deno.env.get('DENO_ENV') ?? 'development';
 config({ path: `./.env.${DENO_ENV}`, export: true });
 
-const PORT = parseInt(Deno.env.get("PORT")) || 80;
-const scoresURL = Deno.env.get("scoresURL");
+const PORT = parseInt(Deno.env.get('PORT')) || 80;
+const scoresURL = Deno.env.get('scoresURL');
 
 const db = new Client(scoresURL);
 await db.connect();
@@ -18,16 +18,16 @@ await db.connect();
 
 const app = new Application();
 const allowedHeaders = [
-  "Authorization",
-  "Content-Type",
-  "Accept",
-  "Origin",
-  "User-Agent",
+  'Authorization',
+  'Content-Type',
+  'Accept',
+  'Origin',
+  'User-Agent',
 ];
-const allowedMethods = ["GET", "POST", "DELETE"];
+const allowedMethods = ['GET', 'POST', 'DELETE'];
 
 const corsConfig = abcCors({
-  origin: "*",
+  origin: '*',
   Headers: allowedHeaders,
   Methods: allowedMethods,
   credentials: true,
@@ -36,41 +36,43 @@ const corsConfig = abcCors({
 app.use(corsConfig);
 
 app
-  .get("/", home)
-  .get("/scores", getScores)
-  .post("/scores", postScore)
-  .delete("/scores/:id", deleteScore)
+  .get('/', home)
+  .get('/scores', getScores)
+  .post('/scores', postScore)
+  .delete('/scores/:id', deleteScore)
   .start({ port: PORT });
 
 async function home(server) {
-  server.json({ response: "server running" }, 200);
+  server.json({ response: 'server running' }, 200);
 }
 
 async function getScores(server) {
   const conditionals = server.queryParams;
-  const paramsPresent = JSON.stringify(conditionals) !== "{}";
+  const paramsPresent = JSON.stringify(conditionals) !== '{}';
   let query = `SELECT * FROM scores`;
 
   if (paramsPresent) {
     const paramsAreValid = checkValidParams(conditionals);
     if (paramsAreValid) {
-      query += " WHERE ";
+      query += ' WHERE ';
       let paramCounter = 1;
 
       Object.keys(conditionals).forEach((param, i) => {
-        query += `${i > 0 ? " AND" : ""} ${param} = $${paramCounter}`;
+        query += `${i > 0 ? ' AND' : ''} ${param} = $${paramCounter}`;
         paramCounter++;
       });
     } else {
       return server.json(
         {
           response:
-            "Invalid query params. Allowed params are username, word or date",
+            'Invalid query params. Allowed params are username, word or date',
         },
         400
       );
     }
   }
+
+  query += `ORDER BY score ASC, date DESC`;
 
   const scores = (
     await db.queryObject({
@@ -83,7 +85,7 @@ async function getScores(server) {
 }
 
 function checkValidParams(conditionals) {
-  const validParams = ["username", "word", "date"];
+  const validParams = ['username', 'word', 'date'];
   const allKeysInTable = Object.keys(conditionals).every((key) =>
     validParams.includes(key)
   );
@@ -96,7 +98,7 @@ async function postScore(server) {
     return server.json(
       {
         response:
-          "Item(s) missing, need username, word and score for a valid request.",
+          'Item(s) missing, need username, word and score for a valid request.',
       },
       400
     );
@@ -108,7 +110,7 @@ async function postScore(server) {
                  ($1, current_timestamp, $2, $3)
                 ;`;
   await db.queryArray({ text: query, args: [score, word, username] });
-  return server.json({ response: "score successfully added" }, 200);
+  return server.json({ response: 'score successfully added' }, 200);
 }
 
 // Used in test file to remove test score
@@ -116,7 +118,7 @@ async function deleteScore(server) {
   const { id } = server.params;
   const query = `DELETE FROM scores WHERE id = $1`;
   await db.queryArray({ text: query, args: [id] });
-  return server.json({ response: "score entry deleted" }, 200);
+  return server.json({ response: 'score entry deleted' }, 200);
 }
 
 console.log(`Server running on http://localhost:${PORT}`);
